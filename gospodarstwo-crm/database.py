72333f1cfa0c9cbc14e461b57caa5038195eee17
rpotorski-cmd <rpotorski@gmail.com>
2026-03-01 11:54:19 +0100

    Update database.py

--- gospodarstwo-crm/database.py
+++ gospodarstwo-crm/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
