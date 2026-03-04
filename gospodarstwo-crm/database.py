28b638166b26f8d1a0576d063ed5aa97e0a55f87
rpotorski-cmd <rpotorski@gmail.com>
2026-03-04 19:40:05 +0100

    Update database.py

--- gospodarstwo-crm/database.py
+++ gospodarstwo-crm/database.py
@@ -11,10 +11,13 @@ engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+def init_db():
+    from models import Base as ModelsBase
+    ModelsBase.metadata.create_all(bind=engine)
